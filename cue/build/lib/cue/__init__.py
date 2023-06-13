5dc69178cab9ecb0730b95d8f66821f1014ba64b
yi-jia-li <yzl466@psu.edu>
2023-06-13 15:21:12 -0400

    delete old files

--- cue/build/lib/cue/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import constants
-from . import utils
-from . import cont_pca
-from . import line_pca
-from . import nn
-#from . import continuum
-#from . import line 
--- cue/build/lib/src/cue/constants.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-c = 2.9979e18 #ang/s
-Lsun = 3.828e+33 #erg/s
-h = 6.626e-27 #erg/s
-    
-HeI_edge = 1e8/198310.66637
-HeII_edge = 1e8/438908.8789
-OII_edge = 1e8/283270.9
